c449d8ae48745533517cfc1477ab9d95df591802
cyarp <cyarp@eecs.berkeley.edu>
2015-01-07 13:48:43 -0800

    More work on golay.  Now have full preamble for both control and SC frame types

--- golay/golay.m
+++ golay/golay.m
@@ -19,6 +19,11 @@ Gb_32  = [-1, -1, -1, -1, -1, +1, -1, +1, +1, +1, -1, -1, -1, +1, +1, -1, +1, +1
 D_32   = [ 1,  4,  8,  2, 16];
 W_32   = [-1,  1, -1,  1, -1];
 
+Gu_512 = cat(2, -Gb_128, -Ga_128, Gb_128, -Ga_128);
+Gv_512 = cat(2, -Gb_128, Ga_128, -Gb_128, -Ga_128);
+
+Gv_128 = Gv_512(1:1:128);
+
 %From 802.11ad - Can Reconstruct Ga and Gb
 %A0 (n)= delta(n)
 %B0 (n)= delta(n)
@@ -37,25 +42,39 @@ W_32   = [-1,  1, -1,  1, -1];
 Tc = 1; % This is a fake Ts (not the one used in 802.11ad)
 
 %% Golay Waveform
-nSTFRep = 0:1:(16*128-1);
-nSTFNeg = (16*128):1:(17*128-1);
+nSC_STFRep = 0:1:(16*128-1);
+nSC_STFNeg = (16*128):1:(17*128-1);
+
+nCTRL_STFRep = 0:1:(48*128-1);
+nCTRL_STFNeg = (48*128):1:(49*128-1);
+nCTRL_STFFin = (49*128):1:(50*128-1);
 
 %Complex Baseband Preamble Signal
-xSTF = cat(2, Ga_128(mod(nSTFRep, 128)+1), -Ga_128(mod(nSTFNeg, 128)+1)); %+1 is for matlab
-rSTF = cat(2, Ga_128(mod(nSTFRep, 128)+1).*exp(j*pi*nSTFRep/2), -Ga_128(mod(nSTFNeg, 128)+1).*exp(j*pi*nSTFNeg/2)); %+1 is for matlab
+xSC_STF   = cat(2, Ga_128(mod(nSC_STFRep, 128)+1), -Ga_128(mod(nSC_STFNeg, 128)+1)); %+1 is for matlab
+xCTRL_STF = cat(2, Gb_128(mod(nCTRL_STFRep, 128)+1), -Gb_128(mod(nCTRL_STFNeg, 128)+1), Ga_128(mod(nCTRL_STFFin, 128)+1)); %+1 is for matlab
+xSC_CEF   = cat(2, Gu_512, Gv_512, Gv_128);
+xCTRL_CEF = xSC_CEF;
+xSC_PRE   = cat(2, xSC_STF, xSC_CEF);
+xCTRL_PRE = cat(2, xCTRL_STF, xCTRL_CEF);
 
-xSTF = transpose(xSTF);
-rSTF = transpose(rSTF);
-
-rSTF_I = real(xSTF);
-rSTF_Q = imag(xSTF);
+xSC_STF   = transpose(xSC_STF);
+xCTRL_STF = transpose(xCTRL_STF);
+xSC_CEF   = transpose(xSC_CEF);
+xCTRL_CEF = transpose(xCTRL_CEF);
+xSC_PRE   = transpose(xSC_PRE);
+xCTRL_PRE = transpose(xCTRL_PRE);
 
 simX.time = [];
-simX.signals.values = xSTF;
+simX.signals.values = xCTRL_PRE;
 simX.signals.dimensions = 1;
 
+rSC_STF   = cat(2, Ga_128(mod(nSC_STFRep, 128)+1).*exp(j*pi*nSC_STFRep/2), -Ga_128(mod(nSC_STFNeg, 128)+1).*exp(j*pi*nSC_STFNeg/2)); %+1 is for matlab
+rSC_STF = transpose(rSC_STF);
+rSC_STF_I = real(xSC_STF);
+rSC_STF_Q = imag(xSC_STF);
+
 figure;
-plot(rSTF);
+plot(rSC_STF);
 
 
 %% Generate Sequence
@@ -68,7 +87,7 @@ disp(mat2str(c(2,:)));
 
 %% Test Correlation
 
-input = xSTF;
+input = xSC_PRE;
 input = transpose(input);
 %input = genA;
 input = cat(2, zeros(1, 1000), input);
@@ -81,12 +100,30 @@ c = golayCorrelate(input, W, D);
 
 figure
 subplot(2, 1, 1);
-plot(c(1, :))
+plot(c(1, :));
+a1 = gca;
 title('Ca');
-xlabel('Sample')
-ylabel('Correlator Output')
+xlabel('Sample');
+ylabel('Correlator Output');
 subplot(2, 1, 2);
-plot(c(2, :))
-title('Cb')
-xlabel('Sample')
-ylabel('Correlator Output')
+plot(c(2, :));
+a2 = gca;
+title('Cb');
+xlabel('Sample');
+ylabel('Correlator Output');
+
+%match axes
+if(a1.YLim(2) - a1.YLim(1) > a2.YLim(2) - a2.YLim(1))
+    a2.YLim = a1.YLim;
+else
+    a1.YLim = a2.YLim;
+end
+
+figure
+plot(c(1, :));
+hold all;
+plot(c(2, :));
+title('Correlators Superimposed');
+xlabel('Sample');
+ylabel('Correlator Output');
+legend('Ca', 'Cb');
