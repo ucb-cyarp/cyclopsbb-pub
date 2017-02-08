21012d2c74849d77fd58b241e1982ccaeaa9cf53
Christopher Yarp <cyarp@eecs.berkeley.edu>
2017-02-07 18:23:54 -0800

    Changed FPGA processor code to decouple preamble from message

--- baseband/pack_preamble.m
+++ baseband/pack_preamble.m
@@ -1,6 +1,6 @@
-lead = zeros(16, 1);
 
-pre = cat(1, lead, xCTRL_PRE_adj, guard, startWord);
+
+pre = xCTRL_PRE_adj;
 packed_word = 32;
 
 packed = zeros(1, ceil(length(pre)/packed_word));
@@ -8,4 +8,4 @@ for i = 0:1:(length(pre)-1)
     packed(idivide(i,int32(packed_word))+1) = bitor(packed(idivide(i,int32(packed_word))+1), (pre(i+1)*2^mod(i,packed_word)));
 end
 
-disp(mat2str(dec2bin(packed)));
+disp(mat2str(dec2hex(packed,8)));
