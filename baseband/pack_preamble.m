e38b98f10883c617be45941d1d1639c499436e67
Christopher Yarp <cyarp@eecs.berkeley.edu>
2017-09-10 16:01:52 -0700

    Change method of exporting preamble

--- baseband/pack_preamble.m
+++ baseband/pack_preamble.m
@@ -1,12 +1,13 @@
-
-
-pre = xCTRL_PRE_adj;
+pre = x_PRE_adj;
 packed_word = 32;
 
 packed = zeros(1, ceil(length(pre)/packed_word));
 for i = 0:1:(length(pre)-1)
     packed(idivide(i,int32(packed_word))+1) = bitor(packed(idivide(i,int32(packed_word))+1), (pre(i+1)*2^mod(i,packed_word)));
 end
+
+disp(['Length Bytes: ' num2str(ceil(length(pre)/8))]);
+disp(['Length Bytes: ' num2str(ceil(length(pre)/packed_word))]);
 
 disp(mat2str(dec2hex(packed,8)));
 disp(mat2str(packed));
