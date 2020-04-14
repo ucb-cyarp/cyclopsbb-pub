922166d0d22eba7adc50609ff91a97f78743f449
Christopher Yarp <cyarp@eecs.berkeley.edu>
2020-04-14 12:28:42 -0700

    Improved simulation by creating vectors specifying when the output of the Tx should be 0.  This resulted in a more realistic scenario between frame transmissions.

--- baseband/createTestVectors.m
+++ baseband/createTestVectors.m
@@ -1,4 +1,4 @@
-function [simX, modX] = createTestVectors(radix, testMsg, testTextTrunkRadix, x_PRE, x_PRE_adj, header_len_bytes, bitsPerSymbolHeader, frame_len_bytes, bitsPerSymbol, after, pad_first)
+function [simX, modX, zeroX] = createTestVectors(radix, testMsg, testTextTrunkRadix, x_PRE, x_PRE_adj, header_len_bytes, bitsPerSymbolHeader, frame_len_bytes, bitsPerSymbol, after, pad_first)
 %pad some 0's to the front (simulate what occurs in the FPGA given the
 %modulator)
 
@@ -18,6 +18,7 @@ headerMask = zeros(header_len_bytes*8/bitsPerSymbolHeader, 1);
 packetMask = ones(frame_len_bytes*8/bitsPerSymbol, 1).*modType;
 afterMask = zeros(length(after), 1);
 modulationMask = cat(1, mod_imperfection, preambleMask, headerMask, packetMask, afterMask);
+zeroMask =  cat(1, true(size(mod_imperfection)), false(size(preambleMask)), false(size(headerMask)), false(size(packetMask)), true(size(afterMask)));
 
 simX.time = [];
 simX.signals.values = cat(1, testMsgFPGA, zeros(round(size(testMsgFPGA)./2)), testMsgFPGA);
@@ -26,6 +27,10 @@ simX.signals.dimensions = 1;
 modX.time = [];
 modX.signals.values = cat(1, modulationMask, zeros(round(size(modulationMask)./2)), modulationMask);
 modX.signals.dimensions = 1;
+
+zeroX.time = [];
+zeroX.signals.values = cat(1, zeroMask, true(round(size(modulationMask)./2)), zeroMask, true(round(size(modulationMask)./2))); %Adding some additional zeros at the end
+zeroX.signals.dimensions = 1;
 
 dataDelay = length(cat(1, x_PRE_adj)) + 1 + 187+360+1;%delay in computing
 idealX.time = [];
