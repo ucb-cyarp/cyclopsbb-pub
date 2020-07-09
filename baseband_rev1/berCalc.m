35ca513044f79f1811a4e4e50061449d17ee87cb
Christopher Yarp <cyarp@eecs.berkeley.edu>
2020-07-09 12:49:07 -0700

    Porting over BER tests for rev1BB

---
+++ baseband_rev1/berCalc.m
@@ -0,0 +1,7 @@
+%% Check BER
+clear; close all; clc;
+
+%% Init Model
+rev1BB_startup;
+
+berCalcPoint;
--- baseband_rev1/getIdealBER.m
+++ baseband_rev1/getIdealBER.m
@@ -1,4 +1,4 @@
-function [EbN0, EsN0, idealBer] = getIdealBER(awgnSNR, effectiveOversmple, radix)
+function [EbN0, EsN0, idealBer, idealEVM] = getIdealBER(awgnSNR, effectiveOversmple, radix)
 %UNTITLED4 Summary of this function goes here
 %   Detailed explanation goes here
 EsN0 = awgnSNR + 10*log10(effectiveOversmple);
@@ -9,5 +9,13 @@ if(radix >= 4)
 else
     idealBer = berawgn(EbN0, 'psk', radix, 'nondiff');
 end
+
+%Compute ideal EVM
+idealEVM = 10^(EsN0/(-20))*100; %TODO: Check.  Keysight equation used SNR but it seems too high (doing better than ideal).  Expect EsN0 is what was really was needed
+%Based on 2.10 of
+%https://ieeexplore.ieee.org/stamp/stamp.jsp?tp=&arnumber=4136960, it looks
+%like there is an assumptuon that Es/N0 = SNR which is not strictly
+%speaking true
+
 end
 
