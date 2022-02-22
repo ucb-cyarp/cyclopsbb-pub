cd60ce0fa5a77963ab5b5fc603652033754f86b5
Christopher Yarp <cyarp@eecs.berkeley.edu>
2022-02-22 11:47:05 -0800

    Adding directory creation and automated log moving to BER script

--- baseband_rev1/runPlotBerParSimUbuntu.sh
+++ baseband_rev1/runPlotBerParSimUbuntu.sh
@@ -3,6 +3,19 @@
 #Collect repo information
 timestamp=$(date +%a%d%b%Y_%H_%M_%S)
 
+dstDir=$1
+
+if [[ -z "${dstDir}" ]]; then
+    echo "Please provide a destination directory"
+    return 1
+elif [[ -d "${dstDir}" ]]; then
+    echo "Provided destination diretory already exists: ${dstDir}"
+    return 1
+else
+    echo "Creating result directory: ${dstDir}"
+    mkdir "${dstDir}"
+fi
+
 git log -1 > "${timestamp}_gitLastCommitDetailed.log"
 git log -1 --format="%H"  > "${timestamp}_gitLastCommit.log"
 git status -b > "${timestamp}_gitStatus.log"
@@ -12,3 +25,13 @@ git diff > "${timestamp}_gitDiff.patch"
 module load matlab
 
 matlab -nodisplay -logfile "${timestamp}_matlab.log" -batch "plotBerParSim;"
+
+#Move Results
+#TODO: Specify exact results of matlab script to move rather than relying on wildcard
+mv "${timestamp}_gitLastCommitDetailed.log" "${dstDir}/."
+mv "${timestamp}_gitLastCommit.log" "${dstDir}/."
+mv "${timestamp}_gitStatus.log" "${dstDir}/."
+mv "${timestamp}_gitDiff.patch" "${dstDir}/."
+mv "${timestamp}_matlab.log" "${dstDir}/."
+
+mv -r "BERvsEbN0_*" "${dstDir}/."
